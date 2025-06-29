cf0bb31501ffb476385e57c7c22dc5cfc173148b
VipinChoudhary1 <vipinchoudhary797@gmail.com>
2025-06-29 17:19:14 +0530

    uploaded the passport authentication using google strategy

---
+++ Google Authentication/auth/passport.js
@@ -0,0 +1,94 @@
+// All the strategies will be made here for that we have to require passport
+const passport=require('passport');
+const User=require('../models/user');
+const LocalStrategy=require('passport-local');
+const FacebookStrategy=require('passport-facebook');
+const GoogleStrategy = require('passport-google-oauth20');
+passport.use(new LocalStrategy({usernameField:'name',passwordField:'password'},
+    async function(username,password,done){
+        try{
+            let user=await User.findOne({username:username})
+            if(!user){return done(null,false);}
+            // if(!user.verifyPassword(password)){return done(null,false)}
+            return done(null,user);
+        }catch(err){
+            return done(err)
+        }
+        }));
+passport.serializeUser( function(user,done){
+    done(null,user.id);
+})
+passport.deserializeUser(async function(id,done){
+    try{
+        let user=await User.findById(id);
+        done(null,user);
+    }catch(err){
+        done(err,user);
+    }
+});
+
+
+
+//FaceBook
+passport.use(new FacebookStrategy({
+    clientID:"835000547966494",
+    clientSecret:"ba5aa3005cac7afae53bde27be1f994a",
+    callbackURL:"http://localhost:8000/login/auth/facebook/callback"
+},async function(accessToken,refreshtoken,profile,cb){
+    // User.findOrCreate({facebookId:profile.id},function(err,user){
+    //     return cb(err,user);
+    // });
+
+
+    try{
+        let user=await User.findOne({
+            fbId:profile.id
+        });
+        if(user) return cb(null,user);
+        user=await User.create({
+            fbaccessToken:accessToken,
+            fbId:profile.id,
+            username:profile.displayName,
+           
+        });
+        cb(null,user);
+    }catch(err){
+        cb(err,false);
+    }
+}
+));
+
+
+//Google Strategy
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET_KEY,
+    callbackURL: "http://localhost:8000/login/auth/google/callback"
+  },
+  async function(accessToken,refreshtoken,profile,cb){
+    // User.findOrCreate({facebookId:profile.id},function(err,user){
+    //     return cb(err,user);
+    // });
+
+
+    try{
+        let user=await User.findOne({
+            fbId:profile.id
+        });
+        if(user) return cb(null,user);
+        user=await User.create({
+            googleaccessToken:accessToken,
+            googleId:profile.id,
+            username:profile.displayName,
+           
+        });
+        cb(null,user);
+    }catch(err){
+        cb(err,false);
+    }
+}
+));
+
+module.exports=passport;
---
+++ Google Authentication/models/user.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose');
+const passport = require('passport');
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+    },
+    password:{
+        type:String,
+    },
+    fbId:{
+        type:String
+    },
+    fbaccessToken:{
+        type:String
+    },
+    googleId:{
+        type:String
+    },
+    googleaccessToken:{
+        type:String
+    }
+
+});
+
+const User=mongoose.model('user',userSchema);
+module.exports=User;
